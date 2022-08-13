b6f9d1068ce8648e461b6826a0b5abe408bbd2ac
Daniel <jackiewicz.daniel@gmail.com>
2022-08-13 08:03:03 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PanelUserComponent } from './panel-user/panel-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ProfileComponent,
+    PanelUserComponent
   ],
   imports: [
     BrowserModule,
